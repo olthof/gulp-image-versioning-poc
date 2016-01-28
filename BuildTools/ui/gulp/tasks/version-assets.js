a9462f7f90a500448e5c257c94351bb070047e14
olthof <timothyolthof@gmail.com>
2016-01-28 23:01:30 +1100

    Added versioning for the images. Doesn't update the image file names in the CSS however.

--- BuildTools/ui/gulp/tasks/version-assets.js
+++ BuildTools/ui/gulp/tasks/version-assets.js
@@ -2,6 +2,11 @@
 
 module.exports = function() {
 
-    
+    var gulp = require('gulp');
+    var rev = require('gulp-rev');
+     
+    return gulp.src('../../images/sprites/*.png')
+        .pipe(rev())
+        .pipe(gulp.dest('../../dist/'));
 
 };
